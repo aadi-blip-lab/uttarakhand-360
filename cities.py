8a38e5bc7924311363d8a9bad059b9a46321b96b
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-03 20:02:40 +0530

    Update cities.py

--- cities.py
+++ cities.py
@@ -1,11 +1,44 @@
 CITIES = [
+    # Major Cities
     "Dehradun, Uttarakhand",
     "Haridwar, Uttarakhand",
     "Rishikesh, Uttarakhand",
+    "Roorkee, Uttarakhand",
+    "Kotdwar, Uttarakhand",
+    "Haldwani, Uttarakhand",
+    "Kashipur, Uttarakhand",
+    "Ramnagar, Uttarakhand",
+
+    # Hill Stations
     "Mussoorie, Uttarakhand",
+    "Nainital, Uttarakhand",
+    "Auli, Uttarakhand",
+    "Kausani, Uttarakhand",
+    "Munsiyari, Uttarakhand",
+    "Chakrata, Uttarakhand",
+
+    # District Headquarters
     "Uttarkashi, Uttarakhand",
     "New Tehri, Uttarakhand",
+    "Pauri Garhwal, Uttarakhand",
     "Chamoli, Uttarakhand",
     "Rudraprayag, Uttarakhand",
-    "Pithoragarh, Uttarakhand"
+    "Almora, Uttarakhand",
+    "Bageshwar, Uttarakhand",
+    "Champawat, Uttarakhand",
+    "Pithoragarh, Uttarakhand",
+
+    # Char Dham Route
+    "Joshimath, Uttarakhand",
+    "Guptkashi, Uttarakhand",
+    "Sonprayag, Uttarakhand",
+    "Govindghat, Uttarakhand",
+
+    # Pilgrimage & Tourism
+    "Devprayag, Uttarakhand",
+    "Karnaprayag, Uttarakhand",
+    "Gopeshwar, Uttarakhand",
+    "Lansdowne, Uttarakhand",
+    "Bhimtal, Uttarakhand",
+    "Mukteshwar, Uttarakhand",
 ]
