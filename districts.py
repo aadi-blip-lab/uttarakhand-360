cac1206b4bf06837a65f389833dd9de64154408b
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-05 11:29:56 +0530

    Create districts.py

---
+++ districts.py
@@ -0,0 +1,99 @@
+# ==========================================================
+# UTTARAKHAND 360
+# Official District Information
+# ==========================================================
+
+DISTRICTS = [
+
+    {
+        "name": "Dehradun",
+        "division": "Garhwal",
+        "headquarters": "Dehradun",
+        "weather_query": "Dehradun, Uttarakhand"
+    },
+
+    {
+        "name": "Haridwar",
+        "division": "Garhwal",
+        "headquarters": "Haridwar",
+        "weather_query": "Haridwar, Uttarakhand"
+    },
+
+    {
+        "name": "Tehri Garhwal",
+        "division": "Garhwal",
+        "headquarters": "New Tehri",
+        "weather_query": "New Tehri, Uttarakhand"
+    },
+
+    {
+        "name": "Uttarkashi",
+        "division": "Garhwal",
+        "headquarters": "Uttarkashi",
+        "weather_query": "Uttarkashi, Uttarakhand"
+    },
+
+    {
+        "name": "Chamoli",
+        "division": "Garhwal",
+        "headquarters": "Gopeshwar",
+        "weather_query": "Gopeshwar, Chamoli, Uttarakhand"
+    },
+
+    {
+        "name": "Rudraprayag",
+        "division": "Garhwal",
+        "headquarters": "Rudraprayag",
+        "weather_query": "Rudraprayag, Uttarakhand"
+    },
+
+    {
+        "name": "Pauri Garhwal",
+        "division": "Garhwal",
+        "headquarters": "Pauri",
+        "weather_query": "Pauri Garhwal, Uttarakhand"
+    },
+
+    {
+        "name": "Almora",
+        "division": "Kumaon",
+        "headquarters": "Almora",
+        "weather_query": "Almora, Uttarakhand"
+    },
+
+    {
+        "name": "Bageshwar",
+        "division": "Kumaon",
+        "headquarters": "Bageshwar",
+        "weather_query": "Bageshwar, Uttarakhand"
+    },
+
+    {
+        "name": "Champawat",
+        "division": "Kumaon",
+        "headquarters": "Champawat",
+        "weather_query": "Champawat, Uttarakhand"
+    },
+
+    {
+        "name": "Nainital",
+        "division": "Kumaon",
+        "headquarters": "Nainital",
+        "weather_query": "Nainital, Uttarakhand"
+    },
+
+    {
+        "name": "Pithoragarh",
+        "division": "Kumaon",
+        "headquarters": "Pithoragarh",
+        "weather_query": "Pithoragarh, Uttarakhand"
+    },
+
+    {
+        "name": "Udham Singh Nagar",
+        "division": "Kumaon",
+        "headquarters": "Rudrapur",
+        "weather_query": "Rudrapur, Uttarakhand"
+    }
+
+]
