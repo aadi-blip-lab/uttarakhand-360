ec6235b11710769ae8a02113766d77dd9ecab397
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-05 14:35:15 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,1 +1,17 @@
+# ==========================================================
+# UTTARAKHAND 360
+# Alert Sources
+# ==========================================================
 
+ALERTS = [
+
+    {
+        "title": "No Active Alerts",
+        "type": "information",
+        "source": "System",
+        "district": "Uttarakhand",
+        "issued": "",
+        "message": "No active government alerts at the moment."
+    }
+
+]
