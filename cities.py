ea820607ccccbb393c080eda38acc3efb37a74ec
aadi-blip-lab <aadipnwr@gmail.com>
2026-07-05 11:01:33 +0530

    Update cities.py

--- cities.py
+++ cities.py
@@ -1,44 +1,105 @@
-CITIES = [
-    # Major Cities
+# ==========================================================
+# UTTARAKHAND 360
+# Master Location List
+# ==========================================================
+
+# ----------------------------------------------------------
+# OFFICIAL DISTRICT HEADQUARTERS (13)
+# ----------------------------------------------------------
+
+DISTRICT_HQ = [
     "Dehradun, Uttarakhand",
     "Haridwar, Uttarakhand",
+    "New Tehri, Uttarakhand",
+    "Uttarkashi, Uttarakhand",
+    "Pauri Garhwal, Uttarakhand",
+    "Rudraprayag, Uttarakhand",
+    "Gopeshwar, Chamoli, Uttarakhand",
+    "Almora, Uttarakhand",
+    "Bageshwar, Uttarakhand",
+    "Champawat, Uttarakhand",
+    "Nainital, Uttarakhand",
+    "Pithoragarh, Uttarakhand",
+    "Rudrapur, Uttarakhand"
+]
+
+# ----------------------------------------------------------
+# MAJOR CITIES & TOWNS
+# ----------------------------------------------------------
+
+MAJOR_TOWNS = [
     "Rishikesh, Uttarakhand",
     "Roorkee, Uttarakhand",
     "Kotdwar, Uttarakhand",
     "Haldwani, Uttarakhand",
     "Kashipur, Uttarakhand",
     "Ramnagar, Uttarakhand",
+    "Srinagar Garhwal, Uttarakhand"
+]
 
-    # Hill Stations
+# ----------------------------------------------------------
+# HILL STATIONS
+# ----------------------------------------------------------
+
+HILL_STATIONS = [
     "Mussoorie, Uttarakhand",
-    "Nainital, Uttarakhand",
     "Auli, Uttarakhand",
+    "Joshimath, Uttarakhand",
     "Kausani, Uttarakhand",
     "Munsiyari, Uttarakhand",
+    "Mukteshwar, Uttarakhand",
+    "Bhimtal, Uttarakhand",
+    "Sattal, Uttarakhand",
+    "Naukuchiatal, Uttarakhand",
+    "Dhanaulti, Uttarakhand",
+    "Kanatal, Uttarakhand",
     "Chakrata, Uttarakhand",
+    "Lansdowne, Uttarakhand"
+]
 
-    # District Headquarters
-    "Uttarkashi, Uttarakhand",
-    "New Tehri, Uttarakhand",
-    "Pauri Garhwal, Uttarakhand",
-    "Chamoli, Uttarakhand",
-    "Rudraprayag, Uttarakhand",
-    "Almora, Uttarakhand",
-    "Bageshwar, Uttarakhand",
-    "Champawat, Uttarakhand",
-    "Pithoragarh, Uttarakhand",
+# ----------------------------------------------------------
+# CHAR DHAM & PILGRIMAGE
+# ----------------------------------------------------------
 
-    # Char Dham Route
-    "Joshimath, Uttarakhand",
+CHAR_DHAM = [
+    "Badrinath, Uttarakhand",
+    "Kedarnath, Uttarakhand",
+    "Gangotri, Uttarakhand",
+    "Yamunotri, Uttarakhand",
     "Guptkashi, Uttarakhand",
     "Sonprayag, Uttarakhand",
+    "Gaurikund, Uttarakhand",
     "Govindghat, Uttarakhand",
+    "Harsil, Uttarakhand",
+    "Barkot, Uttarakhand",
+    "Janki Chatti, Uttarakhand",
+    "Hemkund Sahib, Uttarakhand",
+    "Mana Village, Uttarakhand"
+]
 
-    # Pilgrimage & Tourism
+# ----------------------------------------------------------
+# TREKKING & TOURISM
+# ----------------------------------------------------------
+
+TOURISM = [
+    "Valley of Flowers, Uttarakhand",
     "Devprayag, Uttarakhand",
     "Karnaprayag, Uttarakhand",
-    "Gopeshwar, Uttarakhand",
-    "Lansdowne, Uttarakhand",
-    "Bhimtal, Uttarakhand",
-    "Mukteshwar, Uttarakhand",
+    "Lohajung, Uttarakhand",
+    "Sankri, Uttarakhand",
+    "Dharchula, Uttarakhand",
+    "Jim Corbett National Park, Uttarakhand",
+    "Rajaji National Park, Uttarakhand"
 ]
+
+# ----------------------------------------------------------
+# MASTER LIST
+# ----------------------------------------------------------
+
+CITIES = (
+    DISTRICT_HQ +
+    MAJOR_TOWNS +
+    HILL_STATIONS +
+    CHAR_DHAM +
+    TOURISM
+)
